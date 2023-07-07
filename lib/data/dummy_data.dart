4fb7b1f3d0a71a274356674f2f8589cb79285cc8
Shankaraaditya <aditya.20b0121083@abes.ac.in>
2023-07-07 22:11:02 +0530

    category screen complete

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/category.dart';
+
+var availableCategories = [
+  CategoryModal(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  CategoryModal(
+    id: 'c2', 
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  CategoryModal(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  CategoryModal(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  CategoryModal(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  CategoryModal(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  CategoryModal(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  CategoryModal(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  CategoryModal(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  CategoryModal(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
